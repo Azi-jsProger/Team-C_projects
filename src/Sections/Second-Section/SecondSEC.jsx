248d6c40acb04a6c3569a3f300a1951870a537d5
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-06-19 01:17:03 +0600

    testing aziret

--- src/Sections/Second-Section/SecondSEC.jsx
+++ src/Sections/Second-Section/SecondSEC.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import './style.css'
-
-const SecondSEC = () => {
-    return (
-        <div>
-            AZIRET
-=======
 
 const SecondSec = () => {
     return (
         <div>
 
->>>>>>> 9303760 (sections)
         </div>
     );
 };
 
-<<<<<<< HEAD
-export default SecondSEC;
-=======
 export default SecondSec;
->>>>>>> 9303760 (sections)
