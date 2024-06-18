1399be1db8a88a45b052f552507f00f690f4989b
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-06-18 22:15:50 +0600

    folder architectors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
         <div>
-
+            <Header></Header>
+            <Footer></Footer>
         </div>
     );
 };
