26d0c1d18f19f8f3bbf53a046866a3730d4a4f91
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-06-20 19:19:35 +0600

    second and fouth sections

--- src/Sections/Fourth-Section/FouthSEC.jsx
+++ src/Sections/Fourth-Section/FouthSEC.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import New from "../../components/new/New";
+import styles from './style.module.css'
 
-const FouthSec = () => {
-    return (
-        <div>
+const FouthSec = (props) => {
 
-        </div>
-    );
+    const {
+        news
+    } = props
+
+
+    return (<div className={styles.fouth}>
+            <div className='container'>
+                <h2 className={styles.POl}>Полезные статьи:</h2>
+
+                <div className={styles.card}></div>
+
+                {news.map((item, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className={styles.card}
+                        >
+                            <New
+                                image={item.image}
+                                text={item.text}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>);
 };
 
 export default FouthSec;
