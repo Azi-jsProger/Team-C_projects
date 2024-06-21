551e3d75a91065d9f46599a4247a0aa89f44322c
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-06-21 14:35:02 +0600

    create sections

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {axiosInstance} from "./Utils/API/Api";
 import SecondSec from "./Sections/Second-Section/SecondSEC";
-import FouthSEC from "./Sections/Fourth-Section/FouthSEC";
+import FourthSec from "./Sections/Fourth-Section/FouthSEC";
+
 
 const App = () => {
+    const [news, setNews] = useState([]);
 
-    const [news, setNews] = useState([])
     const getNews = async () => {
-        const res = await axiosInstance.get('/news')
-        setNews(res.data)
-    }
+        const res = await axiosInstance.get('/news');
+        setNews(res.data);
+    };
 
     useEffect(() => {
-        getNews()
+        getNews();
     }, []);
-
 
     return (
         <div>
-            <SecondSec></SecondSec>
-
-            <FouthSEC
+            <SecondSec />
+            <FourthSec
                 news={news}
             />
         </div>
--- src/Sections/Fourth-Section/FouthSEC.jsx
+++ src/Sections/Fourth-Section/FouthSEC.jsx
@@ -1,25 +1,19 @@
+import React from 'react';
 import New from "../../components/new/New";
-import styles from './style.module.css'
+import styles from './style.module.css';
 
-const FouthSec = (props) => {
-
+const FourthSec = (props) => {
     const {
         news
-    } = props
+    } = props;
 
-
-    return (<div className={styles.fouth}>
+    return (
+        <div className={styles.fouth}>
             <div className='container'>
                 <h2 className={styles.POl}>Полезные статьи:</h2>
-
-                <div className={styles.card}></div>
-
                 {news.map((item, idx) => {
                     return (
-                        <div
-                            key={idx}
-                            className={styles.card}
-                        >
+                        <div key={idx} className={styles.card}>
                             <New
                                 image={item.image}
                                 text={item.text}
@@ -28,7 +22,8 @@ const FouthSec = (props) => {
                     )
                 })}
             </div>
-        </div>);
+        </div>
+    );
 };
 
-export default FouthSec;
+export default FourthSec;
