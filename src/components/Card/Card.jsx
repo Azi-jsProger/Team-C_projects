3004e78375f45bd79c2b8e80c6e6539b5cb2b7a6
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-06-22 12:07:07 +0600

    bakai

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,15 +12,24 @@ const Card = (props) => {
     return (
         <div className='card'>
             <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
+                // initial={{ opacity: 0, scale: 0.5 }}
+                // animate={{ opacity: 1, scale: 1 }}
+                // transition={{ duration: 0.5 }}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
                 className="circle"
             >
                 <img src={img} alt=""/>
             </motion.div>
 
-            <h3 className='title'>{title}</h3>
+            <motion.h3
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                className='title'>
+                {title}
+            </motion.h3>
         </div>
     );
 };
